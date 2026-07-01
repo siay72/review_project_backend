8828120e80a7961f4a6f6db6ccfa2bca01839507
Siam Hossain <mdsiamh77@gmail.com>
2026-07-01 09:44:25 +0600

    frontend url added

--- app/main.py
+++ app/main.py
@@ -21,6 +21,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://review-client-two.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
